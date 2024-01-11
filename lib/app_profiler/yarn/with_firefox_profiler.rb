88bec3ce826c8be11d2a0f68f1c3fd4f4174af64
Dale Hamel <dale.hamel@shopify.com>
2024-01-11 11:06:10 -0500

    Patch firefox profiler accordingly

--- lib/app_profiler/yarn/with_firefox_profiler.rb
+++ lib/app_profiler/yarn/with_firefox_profiler.rb
@@ -9,14 +9,46 @@ module AppProfiler
         super
         return if firefox_profiler_added?
 
-        yarn("add", AppProfiler.gecko_viewer_package)
+        fetch_firefox_profiler
         yarn("--cwd", "node_modules/firefox-profiler")
+
+        patch_firefox_profiler
+        yarn("--cwd", "node_modules/firefox-profiler", "build-prod")
+        patch_file("node_modules/firefox-profiler/dist/index.html", 'href="locales/en-US/app.ftl"', 'href="/app_profiler/viewer/locales/en-US/app.ftl"')
       end
 
       private
 
       def firefox_profiler_added?
         AppProfiler.root.join("node_modules/firefox-profiler").exist?
+      end
+
+      def fetch_firefox_profiler
+        puts AppProfiler.gecko_viewer_package
+        raise ArgumentError unless AppProfiler.gecko_viewer_package.start_with?("https://github.com")
+        Dir.mktmpdir do |dir|
+          Dir.chdir(dir) do
+            system("git clone #{AppProfiler.gecko_viewer_package} firefox-profiler")
+            package_contents = File.read("firefox-profiler/package.json")
+            package_json = JSON.parse(package_contents)
+            package_json["name"] ||= "firefox-profiler"
+            package_json["version"] ||= "0.0.1"
+            File.write("firefox-profiler/package.json", package_json.to_json)
+          end
+          yarn("add", "#{dir}/firefox-profiler")
+        end
+      end
+
+      def patch_firefox_profiler
+        # Patch the publicPath so that the app can be "mounted" at the right location
+        patch_file("node_modules/firefox-profiler/webpack.config.js", "publicPath: '/'", "publicPath: '/app_profiler/viewer/'")
+        patch_file("node_modules/firefox-profiler/src/app-logic/l10n.js", "fetch(`/locales/", "fetch(`/app_profiler/viewer/locales/")
+      end
+
+      def patch_file(file, find, replace)
+        contents = File.read(file)
+        new_contents = contents.gsub(find, replace)
+        File.write(file, new_contents)
       end
     end
   end
